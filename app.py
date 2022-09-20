8032d85a728112d31d777e5d66daefd84cec9ef7
KJJDSA <kjjdsa@gmail.com>
2022-09-20 16:58:36 +0900

    버튼으로 로컬환경에서 서로 이동이 가능하도록 수정 if문 하나를 사용해 feeling이 happy면  happys 콜렉션에, joy면 joys 콜렉션에 저장되도록 수정 나중에 db통합할 때는 네개 모두 가능하도록 수정할 예정.

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def happy():
 def joy():
     return render_template("joy.html")
 
-@app.route("/happy_post", methods=["POST"])
-def happy_post():
+@app.route("/post", methods=["POST"])
+def post():
     url_receive = request.form['url_give']
     feeling_receive = request.form['feeling_give']
     comment_receive = request.form['comment_give']
@@ -37,7 +37,11 @@ def happy_post():
         'feeling': feeling_receive,
         'comment': comment_receive
     }
-    db.happys.insert_one(doc)
+    if feeling_receive == 'happy':
+        db.happys.insert_one(doc)
+    else:
+        db.joys.insert_one(doc)
+
     return jsonify({'msg': '저장 완료!'})
 
 @app.route("/happy_get", methods=["GET"])
@@ -45,33 +49,33 @@ def happy_get():
     post_list = list(db.happys.find({}, {'_id': False}))
     return jsonify({'posts':post_list})
 
-@app.route("/joy_post", methods=["POST"])
-def joy_post():
-    url_receive = request.form['url_give']
-    feeling_receive = request.form['feeling_give']
-    comment_receive = request.form['comment_give']
-
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    title = soup.select_one('meta[itemprop="name"][content]')['content']
-    # 제목 뽑아오는 코드
-
-    doc = {
-        'title': title,
-        'url': url_receive,
-        'feeling': feeling_receive,
-        'comment': comment_receive
-    }
-    db.joys.insert_one(doc)
-    return jsonify({'msg': '저장 완료!'})
+# @app.route("/joy_post", methods=["POST"])
+# def joy_post():
+#     url_receive = request.form['url_give']
+#     feeling_receive = request.form['feeling_give']
+#     comment_receive = request.form['comment_give']
+#
+#     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+#     data = requests.get(url_receive, headers=headers)
+#
+#     soup = BeautifulSoup(data.text, 'html.parser')
+#
+#     title = soup.select_one('meta[itemprop="name"][content]')['content']
+#     # 제목 뽑아오는 코드
+#
+#     doc = {
+#         'title': title,
+#         'url': url_receive,
+#         'feeling': feeling_receive,
+#         'comment': comment_receive
+#     }
+#     db.joys.insert_one(doc)
+#     return jsonify({'msg': '저장 완료!'})
 
 @app.route("/joy_get", methods=["GET"])
 def joy_get():
-    post_list = list(db.happys.find({}, {'_id': False}))
-    return jsonify({'joys':post_list})
+    post_list = list(db.joys.find({}, {'_id': False}))
+    return jsonify({'posts':post_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
