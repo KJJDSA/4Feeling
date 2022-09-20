3788cd3d31e070f3b783b8ca9134f8ef0943e7e0
KJJDSA <kjjdsa@gmail.com>
2022-09-20 16:31:27 +0900

    html을 happy와 joy로 나눔. 해야할 일: 버튼에 링크 만들기, 고른 기분에 따라 다른 db에 저장하게 하기. 각 페이지 get할때 원하는 기분만 가져오기.

--- app.py
+++ app.py
@@ -10,11 +10,15 @@ import requests
 from bs4 import BeautifulSoup
 
 @app.route('/')
-def main():
-    return render_template("index.html")
+def happy():
+    return render_template("happy.html")
 
-@app.route("/posting", methods=["POST"])
-def music_post():
+@app.route('/joy')
+def joy():
+    return render_template("joy.html")
+
+@app.route("/happy_post", methods=["POST"])
+def happy_post():
     url_receive = request.form['url_give']
     feeling_receive = request.form['feeling_give']
     comment_receive = request.form['comment_give']
@@ -33,13 +37,41 @@ def music_post():
         'feeling': feeling_receive,
         'comment': comment_receive
     }
-    db.posts.insert_one(doc)
+    db.happys.insert_one(doc)
     return jsonify({'msg': '저장 완료!'})
 
-@app.route("/post_get", methods=["GET"])
-def post_get():
-    post_list = list(db.posts.find({}, {'_id': False}))
+@app.route("/happy_get", methods=["GET"])
+def happy_get():
+    post_list = list(db.happys.find({}, {'_id': False}))
     return jsonify({'posts':post_list})
+
+@app.route("/joy_post", methods=["POST"])
+def joy_post():
+    url_receive = request.form['url_give']
+    feeling_receive = request.form['feeling_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    title = soup.select_one('meta[itemprop="name"][content]')['content']
+    # 제목 뽑아오는 코드
+
+    doc = {
+        'title': title,
+        'url': url_receive,
+        'feeling': feeling_receive,
+        'comment': comment_receive
+    }
+    db.joys.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
+
+@app.route("/joy_get", methods=["GET"])
+def joy_get():
+    post_list = list(db.happys.find({}, {'_id': False}))
+    return jsonify({'joys':post_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
