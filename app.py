49294f5a6c524ff23d1b609df69727f1bf8eabfc
KJJDSA <kjjdsa@gmail.com>
2022-09-21 15:12:10 +0900

    메인페이지 최종

--- app.py
+++ app.py
@@ -1,27 +1,146 @@
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
+
+import jwt
+import datetime
+import hashlib
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 
 from pymongo import MongoClient
 client = MongoClient('mongodb://43.201.21.84', 27017, username="test", password="test")
 db = client.forfeeling
+# client = MongoClient('mongodb://43.201.21.84', 27017, username="test", password="test")
+# db = client.forfeeling
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+
 
 @app.route('/')
-def main():
-    return render_template("index.html")
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # jwt토큰을 해독한다
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+        # 요청한 유저정보를 html에 던져준다!
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
 #joy페이지로 이동
 @app.route('/joy')
 def joy():
-    return render_template("joy.html")
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # jwt토큰을 해독한다
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('joy.html', user_info=user_info)
+        # 요청한 유저정보를 html에 던져준다!
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 #happy 페이지로 이동
 @app.route('/happy')
 def happy():
-    return render_template("happy.html")
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # jwt토큰을 해독한다
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('happy.html', user_info=user_info)
+        # 요청한 유저정보를 html에 던져준다!
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 #angry 페이지로 이동
 @app.route('/angry')
 def angry():
-    return render_template("angry.html")
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # jwt토큰을 해독한다
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('angry.html', user_info=user_info)
+        # 요청한 유저정보를 html에 던져준다!
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+############################################### 로그인 기능 시작
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    # 로그인할때도 암호화를 진행한다 (랜덤이 아니구나!!!)
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        # result가 있다면 이라는 뜻이다..
+        payload = {
+        # 이게 곧 토큰이 됨.
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지| 60초X60X24
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')  # .decode('utf-8') 왠진몰라도 없으니 잘된다.
+        #이건 토큰을 암호화 하는 것. 그대로 던져준다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    # 회원가입
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    # 패스워드는 해쉬해준 뒤 저장한다!
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""                                          # 프로필 한 마디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    # ID 중복확인
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    # username을 받아서 find_one으로 대조해보고 없으면 success, 있으면 bool로 exists로 바꿔줌.
+    # bool 연산자는 참이라면 뱉고 거짓이면 뱉지 않는다. 거짓이면 자동적으로 result:success만 남겠지만
+    # 만약 참이라면? exsist:exsist 가 뱉어질것이다.
+    return jsonify({'result': 'success', 'exists': exists})
+
+######################### 로그인기능 끝
 
 #포스팅 루트
 @app.route("/posting", methods=["POST"])
